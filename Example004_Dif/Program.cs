c3d9f83542ca9517a46d17488eadda14377910c3
Yuriy <a.yuriy.v@mail.ru>
2022-06-09 16:28:29 +0300

    add Random:;

--- Example004_Dif/Program.cs
+++ Example004_Dif/Program.cs
@@ -1,4 +1,3 @@
-﻿double numberA = 12;
-double numberB = 5;
-double result = numberA / numberB;
-Console.WriteLine(result);
+﻿double numberA = 8 ;
+double numberB = 4 ;
+Console.WriteLine(numberA / numberB);
