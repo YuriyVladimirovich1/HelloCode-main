a6be4ea51c9eb58354c79f2eabd74bc9ad513da6
Yuriy <a.yuriy.v@mail.ru>
2022-06-09 17:01:33 +0300

    add Example005_Condition Ife eLes

---
+++ Example005_ConditionIfeLes/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write(" username!: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine("Oh , you Masha !");
+}
+else
+{
+    Console.Write("hi , ");
+    Console.WriteLine(username );
+}
