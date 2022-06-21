afd7698c44ecf3fca3241091afff0efc15c5058b
Yuriy <a.yuriy.v@mail.ru>
2022-06-21 09:54:12 +0300

    Examples010_MethodArrey

---
+++ Examples010_MethodArrey/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] arrey = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = arrey.Length;
+int find = 18;
+
+int index = 0;
+while (index < n)
+{
+    if(arrey[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
